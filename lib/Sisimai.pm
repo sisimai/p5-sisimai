7c09bf14bd555cad0e47465fb5cd9d7ce5e18920
azumakuniyuki <azumakuniyuki@gmail.com>
2014-09-13 06:35:23 +0900

    Sisimai 4.1.0

--- lib/Sisimai.pm
+++ lib/Sisimai.pm
@@ -3,7 +3,7 @@ use feature ':5.10';
 use strict;
 use warnings;
 
-our $VERSION = '4.0.2';
+our $VERSION = '4.1.0';
 sub version { return $VERSION }
 sub sysname { 'bouncehammer'  }
 sub libname { 'Sisimai'       }
