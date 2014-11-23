d8f7bfc30dea11e9fd4017f6ae05f27e1cdddf36
azumakuniyuki <azumakuniyuki@gmail.com>
2014-11-23 20:57:07 +0900

    MTA module for Microsft Outlook.com

---
+++ lib/Sisimai/MSP/US/Outlook.pm
@@ -0,0 +1,258 @@
+package Sisimai::MSP::US::Outlook;
+use parent 'Sisimai::MSP';
+use feature ':5.10';
+use strict;
+use warnings;
+
+my $RxMSP = {
+    'from'    => qr/postmaster[@]/,
+    'begin'   => qr/\AThis is an automatically generated Delivery Status Notification/,
+    'error'   => qr/\A[.]+ while talking to .+[:]\z/,
+    'rfc822'  => qr|\AContent-Type: message/rfc822\z|,
+    'endof'   => qr/\A__END_OF_EMAIL_MESSAGE__\z/,
+    'subject' => qr/Delivery Status Notification/,
+};
+
+my $RxErr = {
+    'hostunknown' => [
+        qr/The mail could not be delivered to the recipient because the domain is not reachable/,
+    ],
+};
+
+sub version     { '4.0.0' }
+sub description { 'Microsoft Outlook.com' }
+sub smtpagent   { 'US::Outlook' }
+sub headerlist  { return [ 'X-Message-Delivery', 'X-Message-Info' ] }
+
+sub scan {
+    # @Description  Detect an error from Microsoft Outlook.com
+    # @Param <ref>  (Ref->Hash) Message header
+    # @Param <ref>  (Ref->String) Message body
+    # @Return       (Ref->Hash) Bounce data list and message/rfc822 part
+    my $class = shift;
+    my $mhead = shift // return undef;
+    my $mbody = shift // return undef;
+    my $match = 0;
+
+    $match = 1 if $mhead->{'subject'} =~ $RxMSP->{'subject'};
+    $match = 1 if $mhead->{'x-message-delivery'};
+    $match = 1 if $mhead->{'x-message-info'};
+    return undef unless $match;
+
+    my $dscontents = [];    # (Ref->Array) SMTP session errors: message/delivery-status
+    my $rfc822head = undef; # (Ref->Array) Required header list in message/rfc822 part
+    my $rfc822part = '';    # (String) message/rfc822-headers part
+    my $rfc822next = { 'from' => 0, 'to' => 0, 'subject' => 0 };
+    my $previousfn = '';    # (String) Previous field name
+
+    my $stripedtxt = [ split( "\n", $$mbody ) ];
+    my $recipients = 0;     # (Integer) The number of 'Final-Recipient' header
+    my $connvalues = 0;     # (Integer) Flag, 1 if all the value of $connheader have been set
+    my $connheader = {
+        'date'  => '',      # The value of Arrival-Date header
+        'lhost' => '',      # The value of Reporting-MTA header
+    };
+
+    my $v = undef;
+    my $p = '';
+    push @$dscontents, __PACKAGE__->DELIVERYSTATUS;
+    $rfc822head = __PACKAGE__->RFC822HEADERS;
+
+    for my $e ( @$stripedtxt ) {
+        # Read each line between $RxMSP->{'begin'} and $RxMSP->{'rfc822'}.
+        if( ( $e =~ $RxMSP->{'rfc822'} ) .. ( $e =~ $RxMSP->{'endof'} ) ) {
+            # After "message/rfc822"
+            if( $e =~ m/\A([-0-9A-Za-z]+?)[:][ ]*(.+)\z/ ) {
+                # Get required headers only
+                my $lhs = $1;
+                my $rhs = $2;
+
+                $previousfn = '';
+                next unless grep { lc( $lhs ) eq lc( $_ ) } @$rfc822head;
+
+                $previousfn  = $lhs;
+                $rfc822part .= $e."\n";
+
+            } elsif( $e =~ m/\A[\s\t]+/ ) {
+                # Continued line from the previous line
+                next if $rfc822next->{ lc $previousfn };
+                $rfc822part .= $e."\n" if $previousfn =~ m/\A(?:From|To|Subject)\z/;
+
+            } else {
+                # Check the end of headers in rfc822 part
+                next unless $previousfn =~ m/\A(?:From|To|Subject)\z/;
+                next unless $e =~ m/\A\z/;
+                $rfc822next->{ lc $previousfn } = 1;
+            }
+
+        } else {
+            # Before "message/rfc822"
+            next unless ( $e =~ $RxMSP->{'begin'} ) .. ( $e =~ $RxMSP->{'rfc822'} );
+            next unless length $e;
+
+            if( $connvalues == scalar( keys %$connheader ) ) {
+                # This is an automatically generated Delivery Status Notification.
+                #
+                # Delivery to the following recipients failed.
+                #
+                #      kijitora@example.jp
+                #
+                # Final-Recipient: rfc822;kijitora@example.jp
+                # Action: failed
+                # Status: 5.2.2
+                # Diagnostic-Code: smtp;550 5.2.2 <kijitora@example.jp>... Mailbox Full
+                $v = $dscontents->[ -1 ];
+
+                if( $e =~ m/\AFinal-Recipient:[ ]*rfc822;[ ]*([^ ]+)\z/i ) {
+                    # Final-Recipient: rfc822;kijitora@example.jp
+                    if( length $v->{'recipient'} ) {
+                        # There are multiple recipient addresses in the message body.
+                        push @$dscontents, __PACKAGE__->DELIVERYSTATUS;
+                        $v = $dscontents->[ -1 ];
+                    }
+                    $v->{'recipient'} = $1;
+                    $recipients++;
+
+                } elsif( $e =~ m/\AAction:[ ]*(.+)\z/i ) {
+                    # Action: failed
+                    $v->{'action'} = lc $1;
+
+                } elsif( $e =~ m/\AStatus:[ ]*(\d[.]\d+[.]\d+)/i ) {
+                    # Status:5.2.0
+                    $v->{'status'} = $1;
+
+                } else {
+
+                    if( $e =~ m/\ADiagnostic-Code:[ ]*(.+?);[ ]*(.+)\z/i ) {
+                        # Diagnostic-Code: SMTP; 550 5.1.1 <userunknown@example.jp>... User Unknown
+                        $v->{'spec'} = uc $1;
+                        $v->{'diagnosis'} = $2;
+
+                    } elsif( $p =~ m/\ADiagnostic-Code:[ ]*/i && $e =~ m/\A[\s\t]+(.+)\z/ ) {
+                        # Continued line of the value of Diagnostic-Code header
+                        $v->{'diagnosis'} .= ' '.$1;
+                        $e = 'Diagnostic-Code: '.$e;
+                    }
+                }
+
+            } else {
+                # Reporting-MTA: dns;BLU004-OMC3S13.hotmail.example.com
+                # Received-From-MTA: dns;BLU436-SMTP66
+                # Arrival-Date: Fri, 21 Nov 2014 14:17:34 -0800
+                if( $e =~ m/\AReporting-MTA:[ ]*dns;[ ]*(.+)\z/i ) {
+                    # Reporting-MTA: dns;BLU004-OMC3S13.hotmail.example.com
+                    next if length $connheader->{'lhost'};
+                    $connheader->{'lhost'} = $1;
+                    $connvalues++;
+
+                } elsif( $e =~ m/\AArrival-Date:[ ]*(.+)\z/i ) {
+                    # Arrival-Date: Wed, 29 Apr 2009 16:03:18 +0900
+                    next if length $connheader->{'date'};
+                    $connheader->{'date'} = $1;
+                    $connvalues++;
+                }
+            }
+        } # End of if: rfc822
+
+    } continue {
+        # Save the current line for the next loop
+        $p = $e;
+        $e = '';
+    }
+
+    return undef unless $recipients;
+    require Sisimai::String;
+    require Sisimai::RFC5322;
+
+    for my $e ( @$dscontents ) {
+        # Set default values if each value is empty.
+        map { $e->{ $_ } ||= $connheader->{ $_ } || '' } keys %$connheader;
+
+        if( scalar @{ $mhead->{'received'} } ) {
+            # Get localhost and remote host name from Received header.
+            my $r = $mhead->{'received'};
+            $e->{'lhost'} ||= shift @{ Sisimai::RFC5322->received( $r->[0] ) };
+            $e->{'rhost'} ||= pop @{ Sisimai::RFC5322->received( $r->[-1] ) };
+        }
+
+        $e->{'spec'}    ||= 'SMTP';
+        $e->{'agent'}   ||= __PACKAGE__->smtpagent;
+        $e->{'diagnosis'} = Sisimai::String->sweep( $e->{'diagnosis'} );
+
+        SESSION: for my $r ( keys %$RxErr ) {
+            # Verify each regular expression of session errors
+            PATTERN: for my $rr ( @{ $RxErr->{ $r } } ) {
+                # Check each regular expression
+                next(PATTERN) unless $e->{'diagnosis'} =~ $rr;
+                $e->{'reason'} = $r;
+                last(SESSION);
+            }
+        }
+
+        if( length( $e->{'status'} ) == 0 || $e->{'status'} =~ m/\A\d[.]0[.]0\z/ ) {
+            # There is no value of Status header or the value is 5.0.0, 4.0.0
+            my $r = Sisimai::RFC3463->getdsn( $e->{'diagnosis'} );
+            $e->{'status'} = $r if length $r;
+        }
+    }
+    return { 'ds' => $dscontents, 'rfc822' => $rfc822part };
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sisimai::MSP::US::Outlook - bounce mail parser class for Outlook.com.
+
+=head1 SYNOPSIS
+
+    use Sisimai::MSP::US::Outlook;
+
+=head1 DESCRIPTION
+
+Sisimai::MSP::US::Outlook parses a bounce email which created by Microsoft 
+Outlook.com. Methods in the module are called from only Sisimai::Message.
+
+=head1 CLASS METHODS
+
+=head2 C<B<version()>>
+
+C<version()> returns the version number of this module.
+
+    print Sisimai::MSP::US::Outlook->version;
+
+=head2 C<B<description()>>
+
+C<description()> returns description string of this module.
+
+    print Sisimai::MSP::US::Outlook->description;
+
+=head2 C<B<smtpagent()>>
+
+C<smtpagent()> returns MSP name.
+
+    print Sisimai::MSP::US::Outlook->smtpagent;
+
+=head2 C<B<scan( I<header data>, I<reference to body string>)>>
+
+C<scan()> method parses a bounced email and return results as a array reference.
+See Sisimai::Message for more details.
+
+=head1 AUTHOR
+
+azumakuniyuki
+
+=head1 COPYRIGHT
+
+Copyright (C) 2014 azumakuniyuki E<lt>perl.org@azumakuniyuki.orgE<gt>,
+All Rights Reserved.
+
+=head1 LICENSE
+
+This software is distributed under The BSD 2-Clause License.
+
+=cut
+
