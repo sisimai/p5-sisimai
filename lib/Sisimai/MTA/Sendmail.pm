90753b1df457705da7ed44737c7d5adc1a1fc2e0
azumakuniyuki <azumakuniyuki@gmail.com>
2015-01-19 14:25:20 +0900

    Improved code for capturing the value of each email header

--- lib/Sisimai/MTA/Sendmail.pm
+++ lib/Sisimai/MTA/Sendmail.pm
@@ -18,7 +18,7 @@ my $RxMTA = {
     'subject' => qr/(?:see transcript for details\z|\AWarning: )/,
 };
 
-sub version     { '4.0.19' }
+sub version     { '4.0.20' }
 sub description { 'V8Sendmail: /usr/sbin/sendmail' }
 sub smtpagent   { 'Sendmail' }
 
@@ -66,16 +66,15 @@ sub scan {
         # Read each line between $RxMTA->{'begin'} and $RxMTA->{'rfc822'}.
         if( ( $e =~ $RxMTA->{'rfc822'} ) .. ( $e =~ $RxMTA->{'endof'} ) ) {
             # After "message/rfc822"
-            if( $e =~ m/\A([-0-9A-Za-z]+?)[:][ ]*(.+)\z/ ) {
+            if( $e =~ m/\A[-0-9A-Za-z]+?[:][ ]*.+\z/ ) {
                 # Get required headers only
-                my $lhs = $1;
-                my $rhs = $2;
-                my $whs = lc $lhs;
+                my @set = split( ':', $e, 2 );
+                my $lhs = lc $set[0];
 
                 $previousfn = '';
-                next unless grep { $whs eq lc( $_ ) } @$rfc822head;
+                next unless grep { $lhs eq lc( $_ ) } @$rfc822head;
 
-                $previousfn  = $lhs;
+                $previousfn  = $set[0];
                 $rfc822part .= $e."\n";
 
             } elsif( $e =~ m/\A[\s\t]+/ ) {
