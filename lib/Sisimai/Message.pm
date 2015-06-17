9ea71aeaa6362fd632f45609fd030989c139dad7
azumakuniyuki <perl.org@azumakuniyuki.org>
2015-06-18 05:24:48 +0900

    Tiny code improvement for getting headers in the original message

--- lib/Sisimai/Message.pm
+++ lib/Sisimai/Message.pm
@@ -228,7 +228,9 @@ sub resolve {
             require Sisimai::MIME;
 
             # Convert from string to hash reference
-            my $v          = $bouncedata->{'rfc822'} || $bodystring;
+            my $v =  $bouncedata->{'rfc822'} || $bodystring;
+               $v =~ s/^[>]+[ ]//mg;
+
             my @rfc822text = split( "\n", $v );
             my $rfc822head = Sisimai::MTA->RFC822HEADERS;
             my $previousfn = ''; # Previous field name
