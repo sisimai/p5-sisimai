13ad7c2929e67d63813d5b230782e7233717f578
azumakuniyuki <perl.org@azumakuniyuki.org>
2015-11-03 19:26:10 +0900

    Improved code as a precaution for issue #89

--- lib/Sisimai/MTA/MailMarshalSMTP.pm
+++ lib/Sisimai/MTA/MailMarshalSMTP.pm
@@ -35,6 +35,9 @@ sub scan {
     my $rfc822next = { 'from' => 0, 'to' => 0, 'subject' => 0 };
     my $previousfn = '';    # (String) Previous field name
 
+    my $readcursor = 0;     # (Integer) Points the current cursor position
+    my $indicators = __PACKAGE__->INDICATORS;
+
     my $longfields = __PACKAGE__->LONGFIELDS;
     my @stripedtxt = split( "\n", $$mbody );
     my $recipients = 0;     # (Integer) The number of 'Final-Recipient' header
@@ -52,7 +55,17 @@ sub scan {
 
     for my $e ( @stripedtxt ) {
         # Read each line between $RxMTA->{'begin'} and $RxMTA->{'rfc822'}.
-        if( ( $e =~ $RxMTA->{'rfc822'} ) .. ( $e =~ $RxMTA->{'endof'} ) ) {
+        next unless length $e;
+
+        unless( $readcursor ) {
+            $readcursor = $indicators->{'deliverystatus'} if $e =~ $RxMTA->{'begin'};
+        }
+
+        unless( $readcursor & $indicators->{'message-rfc822'} ) {
+            $readcursor = $indicators->{'message-rfc822'} if $e =~ $RxMTA->{'rfc822'};
+        }
+
+        if( $readcursor & $indicators->{'message-rfc822'} ) {
             # After "message/rfc822"
             if( $e =~ m/\A([-0-9A-Za-z]+?)[:][ ]*.+\z/ ) {
                 # Get required headers only
@@ -79,6 +92,7 @@ sub scan {
 
         } else {
             # Before "message/rfc822"
+            next unless $readcursor & $indicators->{'deliverystatus'};
             last if $e =~ $RxMTA->{'rfc822'};
 
             # Your message:
