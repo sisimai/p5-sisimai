cc28b27599d935c96c522679085ea17f52882594
azumakuniyuki <perl.org@azumakuniyuki.org>
2015-11-30 13:18:41 +0900

    The next version of Sisimai is v4.14.0, maybe

--- lib/Sisimai.pm
+++ lib/Sisimai.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::Load '';
 
-our $VERSION = '4.13.2';
+our $VERSION = '4.14.0';
 sub version { return $VERSION }
 sub sysname { 'bouncehammer'  }
 sub libname { 'Sisimai'       }
