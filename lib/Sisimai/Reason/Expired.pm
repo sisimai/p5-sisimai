0e377e4f061a6cf2da66f1e1056978f2e44dac0a
azumakuniyuki <perl.org@azumakuniyuki.org>
2015-12-11 06:45:42 +0900

    Add method comment in true() method

--- lib/Sisimai/Reason/Expired.pm
+++ lib/Sisimai/Reason/Expired.pm
@@ -31,7 +31,14 @@ sub match {
     return 0;
 }
 
-sub true { return undef };
+sub true {
+    # Delivery expired due to connection failure or network error
+    # @param    [Sisimai::Data] argvs   Object to be detected the reason
+    # @return   [Integer]               1: is expired
+    #                                   0: is not expired
+    # @see      http://www.ietf.org/rfc/rfc2822.txt
+    return undef;
+}
 
 1;
 __END__
