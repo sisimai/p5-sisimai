2687dfe311c77986f78be113e9b16164c37fd5e3
azumakuniyuki <perl.org@azumakuniyuki.org>
2015-12-17 09:59:04 +0900

    Removed needless continue block in scan() method, issue #141

--- lib/Sisimai/MTA/qmail.pm
+++ lib/Sisimai/MTA/qmail.pm
@@ -262,11 +262,6 @@ sub scan {
                 $v->{'rhost'} = $1 if $e =~ $ReHost;
             }
         } # End of if: rfc822
-
-    } continue {
-        # Save the current line for the next loop
-        $p = $e;
-        $e = '';
     }
 
     return undef unless $recipients;
