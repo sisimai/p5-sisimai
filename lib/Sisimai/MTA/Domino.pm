07a78890e84f99e24f8fe719b0eef08e8b998c06
azumakuniyuki <perl.org@azumakuniyuki.org>
2015-12-19 03:44:22 +0900

    Remove unused continue block, issue #141

--- lib/Sisimai/MTA/Domino.pm
+++ lib/Sisimai/MTA/Domino.pm
@@ -163,11 +163,6 @@ sub scan {
                 }
             }
         } # End of if: rfc822
-
-    } continue {
-        # Save the current line for the next loop
-        $p = $e;
-        $e = '';
     }
     return undef unless $recipients;
 
