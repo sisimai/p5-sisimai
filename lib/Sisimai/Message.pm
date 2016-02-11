ed2f47c7203b09a5ab3e29caef755701eeee4ce7
azumakuniyuki <azumakuniyuki@gmail.com>
2016-02-11 14:44:14 +0900

    Remove trailing space and tab characters in divideup() method

--- lib/Sisimai/Message.pm
+++ lib/Sisimai/Message.pm
@@ -171,6 +171,7 @@ sub divideup {
     SPLIT_EMAIL: for my $e ( @hasdivided ) {
         # Split email data to headers and a body part.
         $e =~ y/\r\n//d;
+        $e =~ s/[ \t]+\z//; # Remove traling space and tab characters
 
         if( $readcursor & $Indicators->{'endof'} ) {
             # The body part of the email
