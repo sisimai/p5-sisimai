ee60ad5b60d4baf529da37ae2fdfd773ab10b010
azumakuniyuki <azumakuniyuki@gmail.com>
2016-03-24 18:37:11 +0900

    Sisimai v4.17.0p1 for fixed issue #179

--- lib/Sisimai.pm
+++ lib/Sisimai.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::Load '';
 
-our $VERSION = '4.17.0';
+our $VERSION = '4.17.0p1';
 sub version { return $VERSION }
 sub sysname { 'bouncehammer'  }
 sub libname { 'Sisimai'       }
