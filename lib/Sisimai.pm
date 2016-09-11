ce392eccaf643f32232e08b5574ca4dadfeb29b4
azumakuniyuki <azumakuniyuki@gmail.com>
2016-09-12 06:49:13 +0900

    As of present, the next version is 4.18.2

--- lib/Sisimai.pm
+++ lib/Sisimai.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::Load '';
 
-our $VERSION = '4.18.1';
+our $VERSION = '4.18.2';
 sub version { return $VERSION }
 sub sysname { 'bouncehammer'  }
 sub libname { 'Sisimai'       }
