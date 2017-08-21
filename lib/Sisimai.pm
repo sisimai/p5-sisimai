c643418520a899dfb7119a70969673b0224c49cf
azumakuniyuki <azumakuniyuki@gmail.com>
2017-08-21 17:21:36 +0900

    Bump up the version to v4.22

--- lib/Sisimai.pm
+++ lib/Sisimai.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::Load '';
 
-our $VERSION = '4.21.1p1';
+our $VERSION = '4.22.0';
 sub version { return $VERSION }
 sub sysname { 'bouncehammer'  }
 sub libname { 'Sisimai'       }
