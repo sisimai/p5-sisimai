7fe12222b59881bc7a3a4958afe52a3405552ada
azumakuniyuki <azumakuniyuki@gmail.com>
2018-12-11 12:25:37 +0900

    The order of engine modules to be loaded has been updated (Module for the old format has been moved to the end of the list)

--- lib/Sisimai/Bite/Email.pm
+++ lib/Sisimai/Bite/Email.pm
@@ -20,12 +20,12 @@ sub index {
     # @return   [Array] MTA list with order
     return [qw|
         Sendmail Postfix qmail Exim Courier OpenSMTPD Exchange2007 Exchange2003
-        Google Yahoo GSuite Aol Outlook Office365 SendGrid AmazonSES MailRu
-        Yandex MessagingServer Domino Notes ReceivingSES AmazonWorkMail Verizon
-        GMX Bigfoot Facebook Zoho EinsUndEins MessageLabs EZweb KDDI Biglobe
+        Yahoo GSuite Aol Outlook Office365 SendGrid AmazonSES MailRu Yandex 
+        MessagingServer Domino Notes ReceivingSES AmazonWorkMail Verizon GMX
+        Bigfoot Facebook Zoho EinsUndEins MessageLabs EZweb KDDI Biglobe
         ApacheJames McAfee MXLogic MailFoundry IMailServer 
         mFILTER Activehunter InterScanMSS SurfControl MailMarshalSMTP
-        X1 X2 X3 X4 X5 V5sendmail FML
+        X1 X2 X3 X4 X5 V5sendmail FML Google
     |];
 }
 
@@ -33,10 +33,10 @@ sub heads {
     # MTA list which have one or more extra headers
     # @return   [Array] MTA list (have extra headers)
     return [qw|
-        Exim Exchange2007 Exchange2003 Google GSuite Office365 Outlook SendGrid
+        Exim Exchange2007 Exchange2003 GSuite Office365 Outlook SendGrid
         AmazonSES ReceivingSES AmazonWorkMail Aol GMX MailRu MessageLabs Yahoo
         Yandex Zoho EinsUndEins MXLogic McAfee mFILTER EZweb Activehunter IMailServer
-        SurfControl FML
+        SurfControl FML Google
     |];
 }
 
