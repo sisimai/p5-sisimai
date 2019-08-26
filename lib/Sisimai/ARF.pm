cded0d317e0fbe80f203db5531efa8475c214623
azumakuniyuki <azumakuniyuki@gmail.com>
2019-08-26 16:32:31 +0900

    Tiny code improvement #325

--- lib/Sisimai/ARF.pm
+++ lib/Sisimai/ARF.pm
@@ -263,14 +263,13 @@ sub scan {
         if( $rfc822part =~ /^To: (.+[@].+)$/m ) {
             # pick the address from To: header in message/rfc822 part.
             $dscontents->[-1]->{'recipient'} = Sisimai::Address->s3s4($1);
-            $recipients = 1;
 
         } else {
             # Insert pseudo recipient address when there is no valid recipient
             # address in the message.
             $dscontents->[-1]->{'recipient'} = Sisimai::Address->undisclosed('r');
-            $recipients = 1;
         }
+        $recipients = 1;
     }
 
     unless( $rfc822part =~ /\bFrom: [^ ]+[@][^ ]+\b/ ) {
