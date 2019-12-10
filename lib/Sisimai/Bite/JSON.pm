3cc91cd6e2ea7626820d38049d349231f40c5c59
azumakuniyuki <azumakuniyuki@gmail.com>
2019-12-10 13:19:20 +0900

    Add POD section at each module reported at #344. Thanks to @guimard

--- lib/Sisimai/Bite/JSON.pm
+++ lib/Sisimai/Bite/JSON.pm
@@ -9,3 +9,37 @@ sub index      { Sisimai::Lhost->warn('gone'); return ['AmazonSES', 'SendGrid'] 
 sub scan       { Sisimai::Lhost->warn('gone'); return Sisimai::Lhost->make  }
 sub adapt      { Sisimai::Lhost->warn('gone'); return undef }
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sisimai::Bite::JSON - B<OBSOLETED>: Base class for Sisimai::Bite::JSON::*: Cloud Email Delivery
+Services which bounce message is JSON format.
+
+=head1 SYNOPSIS
+
+Do not use this class directly. Use Sisimai::Bite::JSON::* child class such as
+Sisimai::Bite::JSON::SendGrid instead.
+
+=head1 DESCRIPTION
+
+B<This class and child classes in Sisimai/Bite/JSON will be removed at v4.25.5.>
+Sisimai::Bite::JSON is a base class for Sisimai::Bite::JSON::*.
+
+=head1 AUTHOR
+
+azumakuniyuki
+
+=head1 COPYRIGHT
+
+Copyright (C) 2016-2019 azumakuniyuki, All rights reserved.
+
+=head1 LICENSE
+
+This software is distributed under The BSD 2-Clause License.
+
+=cut
+
