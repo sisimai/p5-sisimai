92cd162f4a191a877272355aa21aecce4d0b5686
azumakuniyuki <azumakuniyuki@gmail.com>
2020-01-08 17:53:36 +0900

    Make Sisimai::Lhost::M* faster #354

--- lib/Sisimai/Lhost/mFILTER.pm
+++ lib/Sisimai/Lhost/mFILTER.pm
@@ -5,9 +5,9 @@ use strict;
 use warnings;
 
 my $Indicators = __PACKAGE__->INDICATORS;
+my $ReBackbone = qr/^-------original[ ](?:message|mail[ ]info)/m;
 my $StartingOf = {
     'command'  => ['-------SMTP command'],
-    'rfc822'   => ['-------original message', '-------original mail info'],
     'error'    => ['-------server message'],
 };
 my $MarkingsOf = { 'message' => qr/\A[^ ]+[@][^ ]+[.][a-zA-Z]+\z/ };
@@ -38,91 +38,71 @@ sub make {
     return undef unless $mhead->{'subject'}  eq 'failure notice';
 
     my $dscontents = [__PACKAGE__->DELIVERYSTATUS];
-    my $rfc822list = [];    # (Array) Each line in message/rfc822 part string
-    my $blanklines = 0;     # (Integer) The number of blank lines
+    my $emailsteak = Sisimai::RFC5322->fillet($mbody, $ReBackbone);
     my $readcursor = 0;     # (Integer) Points the current cursor position
     my $recipients = 0;     # (Integer) The number of 'Final-Recipient' header
     my $markingset = { 'diagnosis' => 0, 'command' => 0 };
     my $v = undef;
 
-    for my $e ( split("\n", $$mbody) ) {
-        # Read each line between the start of the message and the start of rfc822 part.
+    for my $e ( split("\n", $emailsteak->[0]) ) {
+        # Read error messages and delivery status lines from the head of the email
+        # to the previous line of the beginning of the original message.
         unless( $readcursor ) {
-            # Beginning of the bounce message or delivery status part
+            # Beginning of the bounce message or message/delivery-status part
             $readcursor |= $Indicators->{'deliverystatus'} if $e =~ $MarkingsOf->{'message'};
         }
+        next unless $readcursor & $Indicators->{'deliverystatus'};
+        next unless length $e;
 
-        unless( $readcursor & $Indicators->{'message-rfc822'} ) {
-            # Beginning of the original message part
-            if( $e eq $StartingOf->{'rfc822'}->[0] || $e eq $StartingOf->{'rfc822'}->[1] ) {
-                $readcursor |= $Indicators->{'message-rfc822'};
-                next;
-            }
-        }
+        # このメールは「m-FILTER」が自動的に生成して送信しています。
+        # メールサーバーとの通信中、下記の理由により
+        # このメールは送信できませんでした。
+        #
+        # 以下のメールアドレスへの送信に失敗しました。
+        # kijitora@example.jp
+        #
+        #
+        # -------server message
+        # 550 5.1.1 unknown user <kijitora@example.jp>
+        #
+        # -------SMTP command
+        # DATA
+        #
+        # -------original message
+        $v = $dscontents->[-1];
 
-        if( $readcursor & $Indicators->{'message-rfc822'} ) {
-            # Inside of the original message part
-            unless( length $e ) {
-                last if ++$blanklines > 1;
-                next;
-            }
-            push @$rfc822list, $e;
-
-        } else {
-            # Error message part
-            next unless $readcursor & $Indicators->{'deliverystatus'};
-            next unless length $e;
-
-            # このメールは「m-FILTER」が自動的に生成して送信しています。
-            # メールサーバーとの通信中、下記の理由により
-            # このメールは送信できませんでした。
-            #
+        if( $e =~ /\A([^ ]+[@][^ ]+)\z/ ) {
             # 以下のメールアドレスへの送信に失敗しました。
             # kijitora@example.jp
-            #
-            #
-            # -------server message
-            # 550 5.1.1 unknown user <kijitora@example.jp>
-            #
+            if( $v->{'recipient'} ) {
+                # There are multiple recipient addresses in the message body.
+                push @$dscontents, __PACKAGE__->DELIVERYSTATUS;
+                $v = $dscontents->[-1];
+            }
+            $v->{'recipient'} = $1;
+            $recipients++;
+
+        } elsif( $e =~ /\A[A-Z]{4}/ ) {
             # -------SMTP command
             # DATA
-            #
-            # -------original message
-            $v = $dscontents->[-1];
+            next if $v->{'command'};
+            $v->{'command'} = $e if $markingset->{'command'};
 
-            if( $e =~ /\A([^ ]+[@][^ ]+)\z/ ) {
-                # 以下のメールアドレスへの送信に失敗しました。
-                # kijitora@example.jp
-                if( $v->{'recipient'} ) {
-                    # There are multiple recipient addresses in the message body.
-                    push @$dscontents, __PACKAGE__->DELIVERYSTATUS;
-                    $v = $dscontents->[-1];
-                }
-                $v->{'recipient'} = $1;
-                $recipients++;
+        } else {
+            # Get error message and SMTP command
+            if( $e eq $StartingOf->{'error'}->[0] ) {
+                # -------server message
+                $markingset->{'diagnosis'} = 1;
 
-            } elsif( $e =~ /\A[A-Z]{4}/ ) {
+            } elsif( $e eq $StartingOf->{'command'}->[0] ) {
                 # -------SMTP command
-                # DATA
-                next if $v->{'command'};
-                $v->{'command'} = $e if $markingset->{'command'};
+                $markingset->{'command'} = 1;
 
             } else {
-                # Get error message and SMTP command
-                if( $e eq $StartingOf->{'error'}->[0] ) {
-                    # -------server message
-                    $markingset->{'diagnosis'} = 1;
-
-                } elsif( $e eq $StartingOf->{'command'}->[0] ) {
-                    # -------SMTP command
-                    $markingset->{'command'} = 1;
-
-                } else {
-                    # 550 5.1.1 unknown user <kijitora@example.jp>
-                    next if index($e, '-') == 0;
-                    next if $v->{'diagnosis'};
-                    $v->{'diagnosis'} = $e;
-                }
+                # 550 5.1.1 unknown user <kijitora@example.jp>
+                next if index($e, '-') == 0;
+                next if $v->{'diagnosis'};
+                $v->{'diagnosis'} = $e;
             }
         } # End of error message part
     }
@@ -144,7 +124,7 @@ sub make {
             $e->{'rhost'} = $ee;
         }
     }
-    return { 'ds' => $dscontents, 'rfc822' => ${ Sisimai::RFC5322->weedout($rfc822list) } };
+    return { 'ds' => $dscontents, 'rfc822' => $emailsteak->[1] };
 }
 
 1;
@@ -191,7 +171,7 @@ azumakuniyuki
 
 =head1 COPYRIGHT
 
-Copyright (C) 2014-2019 azumakuniyuki, All rights reserved.
+Copyright (C) 2014-2020 azumakuniyuki, All rights reserved.
 
 =head1 LICENSE
 
