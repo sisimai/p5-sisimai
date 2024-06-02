1c7355941bc149d920461da9dca770a22b1670c8
azumakuniyuki <azumakuniyuki@gmail.com>
2024-06-02 16:34:11 +0900

    Large scale code refactoring in Sisimai::Rhost::Tencent #524

--- lib/Sisimai/Rhost/Tencent.pm
+++ lib/Sisimai/Rhost/Tencent.pm
@@ -8,35 +8,57 @@ sub get {
     # @param    [Sisimai::Fact] argvs   Parsed email object
     # @return   [String]                The bounce reason at Tencent 
     # @since v4.25.0
+    # @see      https://service.mail.qq.com/detail/122
     my $class = shift;
     my $argvs = shift // return undef;
 
     state $messagesof = {
-        # https://service.mail.qq.com/cgi-bin/help?id=20022
-        'dmarc check failed'                    => 'blocked',
-        'spf check failed'                      => 'blocked',
-        'suspected spam ip'                     => 'blocked',
-        'mail is rejected by recipients'        => 'filtered',
-        'message too large'                     => 'mesgtoobig',
-        'mail content denied'                   => 'spamdetected',
-        'spam is embedded in the email'         => 'spamdetected',
-        'suspected spam'                        => 'spamdetected',
-        'bad address syntax'                    => 'syntaxerror',
-        'connection denied'                     => 'toomanyconn',
-        'connection frequency limited'          => 'toomanyconn',
-        'domain frequency limited'              => 'toomanyconn',
-        'ip frequency limited'                  => 'toomanyconn',
-        'sender frequency limited'              => 'toomanyconn',
-        'mailbox unavailable or access denied'  => 'toomanyconn',
-        'mailbox not found'                     => 'userunknown',
+        'authfailure' => [
+            'spf check failed',         # https://service.mail.qq.com/detail/122/72
+            'dmarc check failed',
+        ],
+        'blocked' => [
+            'suspected bounce attacks', # https://service.mail.qq.com/detail/122/57
+            'suspected spam ip',        # https://service.mail.qq.com/detail/122/66
+            'connection denied',        # https://service.mail.qq.com/detail/122/170
+        ],
+        'mesgtoobig' => [
+            'message too large',        # https://service.mail.qq.com/detail/122/168
+        ],
+        'rejected' => [
+            'suspected spam',                   # https://service.mail.qq.com/detail/122/71
+            'mail is rejected by recipients',   # https://service.mail.qq.com/detail/122/92
+        ],
+        'spandetected' => [
+            'spam is embedded in the email',    # https://service.mail.qq.com/detail/122/59
+            'mail content denied',              # https://service.mail.qq.com/detail/122/171
+        ],
+        'speeding' => [
+            'mailbox unavailable or access denined', # https://service.mail.qq.com/detail/122/166
+        ],
+        'suspend' => [
+            'is a deactivated mailbox', # http://service.mail.qq.com/cgi-bin/help?subtype=1&&id=20022&&no=1000742
+        ],
+        'syntaxerror' => [
+            'bad address syntax', # https://service.mail.qq.com/detail/122/167
+        ],
+        'toomanyconn' => [
+            'ip frequency limited',         # https://service.mail.qq.com/detail/122/172
+            'domain frequency limited',     # https://service.mail.qq.com/detail/122/173
+            'sender frequency limited',     # https://service.mail.qq.com/detail/122/174
+            'connection frequency limited', # https://service.mail.qq.com/detail/122/175
+        ],
+        'userunknown' => [
+            'mailbox not found',  # https://service.mail.qq.com/detail/122/169
+        ],
     };
     my $issuedcode = lc $argvs->{'diagnosticcode'};
     my $reasontext = '';
 
     for my $e ( keys %$messagesof ) {
-        # Try to match the error message with message patterns defined in $MessagesOf
-        next unless index($issuedcode, $e) > -1;
-        $reasontext = $messagesof->{ $e };
+        # Try to find the error message matches with the given error message string
+        next unless grep { index($issuedcode, $_) > -1 } $messagesof->{ $e }->@*;
+        $reasontext = $e;
         last;
     }
     return $reasontext;
