b853a678187a9bec2d6acdde7c1c0866f6dfa81f
azumakuniyuki <azumakuniyuki@gmail.com>
2024-06-04 10:38:15 +0900

    v5.0.3p1

--- lib/Sisimai.pm
+++ lib/Sisimai.pm
@@ -2,7 +2,7 @@ package Sisimai;
 use v5.26;
 use strict;
 use warnings;
-use version; our $VERSION = version->declare('v5.0.3'); our $PATCHLV = 0;
+use version; our $VERSION = version->declare('v5.0.3'); our $PATCHLV = 1;
 sub version { return substr($VERSION->stringify, 1).($PATCHLV > 0 ? 'p'.$PATCHLV : '') }
 sub libname { 'Sisimai' }
 
