18c47fe939f1ce261d69b18f092f7d728a06e25f
azumakuniyuki <azumakuniyuki@gmail.com>
2024-07-09 21:35:36 +0900

    Remove comments in the method #537

--- lib/Sisimai/Message.pm
+++ lib/Sisimai/Message.pm
@@ -24,8 +24,6 @@ sub rise {
     # Constructor of Sisimai::Message
     # @param         [Hash] argvs   Email text data
     # @options argvs [String] data  Entire email message
-    # @options argvs [Array]  load  User defined MTA module list
-    # @options argvs [Array]  order The order of MTA modules
     # @options argvs [Code]   hook  Reference to callback method
     # @return        [Hash]         Structured email data
     #                [Undef]        If each value of the arguments are missing
