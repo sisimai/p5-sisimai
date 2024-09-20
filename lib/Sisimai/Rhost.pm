375c6a38d1026418c895b89471139fc8c8e567d2
azumakuniyuki <azumakuniyuki@gmail.com>
2024-09-20 14:53:49 +0900

    Buf fix in Sisimai::Rhost->get() for checking the domain part

--- lib/Sisimai/Rhost.pm
+++ lib/Sisimai/Rhost.pm
@@ -33,11 +33,10 @@ sub get {
 
     my $rhostmatch = undef;
     my $rhostclass = '';
-
     for my $e ( keys %$RhostClass ) {
         # Try to match with each value of RhostClass
         $rhostmatch   = 1 if grep { index($remotehost, $_) > -1 } $RhostClass->{ $e }->@*;
-        $rhostmatch ||= 1 if grep { index($domainpart, $_) > -1 } $RhostClass->{ $e }->@*;
+        $rhostmatch ||= 1 if grep { index($_, $domainpart) > -1 } $RhostClass->{ $e }->@*;
         next unless $rhostmatch;
 
         $rhostclass = __PACKAGE__.'::'.$e;
