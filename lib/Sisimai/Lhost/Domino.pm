cc2e4baba41ebfa65932e07a44b52ba06ecc45f5
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-22 04:32:36 +0900

    Tiny code improvements ($match)

--- lib/Sisimai/Lhost/Domino.pm
+++ lib/Sisimai/Lhost/Domino.pm
@@ -18,13 +18,9 @@ sub inquire {
     my $class = shift;
     my $mhead = shift // return undef;
     my $mbody = shift // return undef;
-    my $match = 0;
 
-    while(1) {
-        $match ||= 1 if index($mhead->{'subject'}, 'DELIVERY FAILURE:') == 0;
-        $match ||= 1 if index($mhead->{'subject'}, 'DELIVERY_FAILURE:') == 0;
-        last;
-    }
+    my $match = 0; $match ||= 1 if index($mhead->{'subject'}, 'DELIVERY FAILURE:') == 0;
+                   $match ||= 1 if index($mhead->{'subject'}, 'DELIVERY_FAILURE:') == 0;
     return undef unless $match > 0;
 
     state $indicators = __PACKAGE__->INDICATORS;
