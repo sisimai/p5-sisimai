49a1b48a5509b15235629719bb939714ec69da56
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-26 14:08:01 +0900

    #628 Code improvements in Sisimai::Rhost::NTTDOCOMO

--- lib/Sisimai/Rhost/NTTDOCOMO.pm
+++ lib/Sisimai/Rhost/NTTDOCOMO.pm
@@ -18,102 +18,84 @@ sub find {
         'userunknown' => ['550 unknown user'],
     };
     my $statuscode = $argvs->{'deliverystatus'}    || '';
-    my $thecommand = $argvs->{'command'}           || '';
     my $issuedcode = lc $argvs->{'diagnosticcode'} || '';
-    my $reasontext = '';
+    my $thecommand = $argvs->{'command'}           || '';
 
     # Check the value of Status: field, an SMTP Reply Code, and the SMTP Command
-    if( $statuscode eq '5.1.1' || $statuscode eq '5.0.911' ) {
-        #    ----- Transcript of session follows -----
-        # ... while talking to mfsmax.docomo.ne.jp.:
-        # >>> RCPT To:<***@docomo.ne.jp>
-        # <<< 550 Unknown user ***@docomo.ne.jp
-        # 550 5.1.1 <***@docomo.ne.jp>... User unknown
-        # >>> DATA
-        # <<< 503 Bad sequence of commands
-        $reasontext = 'userunknown';
+    #
+    #    ----- Transcript of session follows -----
+    # ... while talking to mfsmax.docomo.ne.jp.:
+    # >>> RCPT To:<***@docomo.ne.jp>
+    # <<< 550 Unknown user ***@docomo.ne.jp
+    # 550 5.1.1 <***@docomo.ne.jp>... User unknown
+    # >>> DATA
+    # <<< 503 Bad sequence of commands
+    # ---------------------------------------------------------------------------------------------
+    #    ----- The following addresses had permanent fatal errors -----
+    # <***@docomo.ne.jp>
+    # (reason: 550 Unknown user ***@docomo.ne.jp)
+    # 
+    #    ----- Transcript of session follows -----
+    # ... while talking to mfsmax.docomo.ne.jp.:
+    # >>> DATA
+    # <<< 550 Unknown user ***@docomo.ne.jp
+    # 554 5.0.0 Service unavailable
+    # ...
+    # Final-Recipient: RFC822; ***@docomo.ne.jp
+    # Action: failed
+    # Status: 5.2.0
+    return 'userunknown' if $statuscode eq '5.1.1' || $statuscode eq '5.0.911';
+    return 'filtered'    if $statuscode eq '5.2.0';
 
-    } elsif( $statuscode eq '5.2.0' ) {
-        #    ----- The following addresses had permanent fatal errors -----
-        # <***@docomo.ne.jp>
-        # (reason: 550 Unknown user ***@docomo.ne.jp)
-        # 
-        #    ----- Transcript of session follows -----
-        # ... while talking to mfsmax.docomo.ne.jp.:
-        # >>> DATA
-        # <<< 550 Unknown user ***@docomo.ne.jp
-        # 554 5.0.0 Service unavailable
-        # ...
-        # Final-Recipient: RFC822; ***@docomo.ne.jp
-        # Action: failed
-        # Status: 5.2.0
-        $reasontext = 'filtered';
-
-    } else {
+    for my $e ( keys %$messagesof ) {
         # The value of "Diagnostic-Code:" field is not empty
-        for my $e ( keys %$messagesof ) {
-            # - The key name is a bounce reason name
-            # - https://github.com/sisimai/go-sisimai/issues/64
-            # - After March 12, 2025, if an error message contains "550 Unknown user", the
-            #   bounce reason will be definitively "userunknown". This is because NTT DOCOMO
-            #   no longer rejects emails via SMTP for domain-specific rejection or specified
-            #   reception filters.
-            next unless grep { index($issuedcode, $_) > -1 } $messagesof->{ $e }->@*;
-            $reasontext = $e;
-            last;
-        }
+        # - The key name is a bounce reason name
+        # - https://github.com/sisimai/go-sisimai/issues/64
+        # - After March 12, 2025, if an error message contains "550 Unknown user", the
+        #   bounce reason will be definitively "userunknown". This is because NTT DOCOMO
+        #   no longer rejects emails via SMTP for domain-specific rejection or specified
+        #   reception filters.
+        return $e if grep { index($issuedcode, $_) > -1 } $messagesof->{ $e }->@*;
     }
-    return $reasontext if length $reasontext;
 
     # A bounce reason did not decide from a status code, an error message.
     if( $statuscode eq '5.0.0' ) {
-        # Status: 5.0.0
-        if( $thecommand eq 'RCPT' ) {
-            # Your message to the following recipients cannot be delivered:
-            #
-            # <***@docomo.ne.jp>:
-            # mfsmax.docomo.ne.jp [203.138.181.112]:
-            # >>> RCPT TO:<***@docomo.ne.jp>
-            # <<< 550 Unknown user ***@docomo.ne.jp
-            # ...
-            #
-            # Final-Recipient: rfc822; ***@docomo.ne.jp
-            # Action: failed
-            # Status: 5.0.0
-            # Remote-MTA: dns; mfsmax.docomo.ne.jp [203.138.181.112]
-            # Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
-            $reasontext = 'userunknown';
-
-        } elsif( $thecommand eq 'DATA' ) {
-            # <***@docomo.ne.jp>: host mfsmax.docomo.ne.jp[203.138.181.240] said:
-            # 550 Unknown user ***@docomo.ne.jp (in reply to end of DATA
-            # command)
-            # ...
-            # Final-Recipient: rfc822; ***@docomo.ne.jp
-            # Original-Recipient: rfc822;***@docomo.ne.jp
-            # Action: failed
-            # Status: 5.0.0
-            # Remote-MTA: dns; mfsmax.docomo.ne.jp
-            # Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
-            $reasontext = 'rejected';
-
-        } else {
-            # Rejected by other SMTP commands: AUTH, MAIL,
-            #   もしもこのブロックを通過するNTTドコモからのエラーメッセージを見つけたら
-            #   https://github.com/sisimai/p5-sisimai/issues からご連絡ねがいます。
-            #
-            #   If you found a error message from mfsmax.docomo.ne.jp which passes this block,
-            #   please open an issue at https://github.com/sisimai/p5-sisimai/issues .
-        }
-    } else {
-        # Status: field is neither 5.0.0 nor values defined in code above
-        #   もしもこのブロックを通過するNTTドコモからのエラーメッセージを見つけたら
-        #   https://github.com/sisimai/p5-sisimai/issues からご連絡ねがいます。
+        # Your message to the following recipients cannot be delivered:
         #
-        #   If you found a error message from mfsmax.docomo.ne.jp which passes this block,
-        #   please open an issue at https://github.com/sisimai/p5-sisimai .
+        # <***@docomo.ne.jp>:
+        # mfsmax.docomo.ne.jp [203.138.181.112]:
+        # >>> RCPT TO:<***@docomo.ne.jp>
+        # <<< 550 Unknown user ***@docomo.ne.jp
+        # ...
+        #
+        # Final-Recipient: rfc822; ***@docomo.ne.jp
+        # Action: failed
+        # Status: 5.0.0
+        # Remote-MTA: dns; mfsmax.docomo.ne.jp [203.138.181.112]
+        # Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
+        # -----------------------------------------------------------------------------------------
+        # <***@docomo.ne.jp>: host mfsmax.docomo.ne.jp[203.138.181.240] said:
+        # 550 Unknown user ***@docomo.ne.jp (in reply to end of DATA
+        # command)
+        # ...
+        # Final-Recipient: rfc822; ***@docomo.ne.jp
+        # Original-Recipient: rfc822;***@docomo.ne.jp
+        # Action: failed
+        # Status: 5.0.0
+        # Remote-MTA: dns; mfsmax.docomo.ne.jp
+        # Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
+        return 'userunknown' if $thecommand eq 'RCPT';
+        return 'rejected'    if $thecommand eq 'DATA';
     }
-    return $reasontext;
+
+    # 1. Rejected by other SMTP commands: AUTH, MAIL,
+    # 2. Status: field is neither 5.0.0 nor values defined in code above
+    #   もしもこのブロックを通過するNTTドコモからのエラーメッセージを見つけたら
+    #   https://github.com/sisimai/p5-sisimai/issues からご連絡ねがいます。
+    #
+    #   If you found a error message from mfsmax.docomo.ne.jp which passes this block,
+    #   please open an issue at https://github.com/sisimai/p5-sisimai/issues .
+    return "";
 }
 
 1;
