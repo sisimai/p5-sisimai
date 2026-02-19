5a708a87fd757a2f252005238b4f2644ca0c99f4
azumakuniyuki <azumakuniyuki@gmail.com>
2026-02-20 05:52:23 +0900

    #647 Reduce error message patterns from Sisimai::Lhost::EZweb

--- lib/Sisimai/Lhost/EZweb.pm
+++ lib/Sisimai/Lhost/EZweb.pm
@@ -36,30 +36,18 @@ sub inquire {
     state $indicators = __PACKAGE__->INDICATORS;
     state $boundaries = ["--------------------------------------------------", "Content-Type: message/rfc822"];
     state $startingof = {"message" => ['The user(s) ', 'Your message ', 'Each of the following', '<']};
-    state $messagesof = {
-        #'notaccept'  => ['The following recipients did not receive this message:'],
-        'expired' => [
-            # Your message was not delivered within 0 days and 1 hours.
-            # Remote host is not responding.
-            'Your message was not delivered within ',
-        ],
-        'mailboxfull' => ['The user(s) account is temporarily over quota'],
-        'onhold'  => ['Each of the following recipients was rejected by a remote mail server'],
-        'suspend' => [
-            # http://www.naruhodo-au.kddi.com/qa3429203.html
-            # The recipient may be unpaid user...?
-            'The user(s) account is disabled.',
-            'The user(s) account is temporarily limited.',
-        ],
-    };
+    state $unpaiduser = [
+        # http://www.naruhodo-au.kddi.com/qa3429203.html
+        # The recipient may be unpaid user...?
+        "The user(s) account is disabled.",
+        "The user(s) account is temporarily limited.",
+    ];
 
     my $fieldtable = Sisimai::RFC1894->FIELDTABLE;
     my $dscontents = [__PACKAGE__->DELIVERYSTATUS]; my $v = undef;
     my $emailparts = Sisimai::RFC5322->part($mbody, $boundaries);
     my $readcursor = 0;     # Points the current cursor position
     my $recipients = 0;     # The number of 'Final-Recipient' header
-    my $substrings = [];    # All the values of "messagesof"
-    map { push @$substrings, $messagesof->{ $_ }->@* } keys %$messagesof;
 
     for my $e ( split("\n", $emailparts->[0]) ) {
         # Read error messages and delivery status lines from the head of the email to the previous
@@ -114,16 +102,6 @@ sub inquire {
             } elsif( index($e, " <<< ") > -1 ) {
                 # <<< 550 ...
                 $v->{"diagnosis"} .= " ".$e;
-
-            } else {
-                # Check error message
-                my $isincluded = 0;
-                if( grep { index($e, $_) > -1 } @$substrings ) {
-                    # Try to find that the line contains any error message text
-                    $v->{"diagnosis"} .= ' '.$e;
-                    $isincluded = 1;
-                }
-                $v->{"diagnosis"} .= " ".$e if $isincluded == 0;
             }
         } # End of error message part
     }
@@ -142,15 +120,7 @@ sub inquire {
 
         } else {
             # There is no X-SPASIGN header or the value of the header is not "NG"
-            FINDREASON: for my $r ( keys %$messagesof ) {
-                # Try to match with each session error message
-                for my $f ( $messagesof->{ $r }->@* ) {
-                    # Check each error message pattern
-                    next unless index($e->{'diagnosis'}, $f) > -1;
-                    $e->{'reason'} = $r;
-                    last FINDREASON;
-                }
-            }
+            $e->{'reason'} = 'suspend' if grep { index($e->{'diagnosis'}, $_) > -1 } $unpaiduser->@*;
         }
         next if $e->{'reason'};
         next if index($e->{'recipient'}, '@ezweb.ne.jp') > 1 || index($e->{'recipient'}, '@au.com') > 1;
@@ -195,7 +165,7 @@ azumakuniyuki
 
 =head1 COPYRIGHT
 
-Copyright (C) 2014-2025 azumakuniyuki, All rights reserved.
+Copyright (C) 2014-2026 azumakuniyuki, All rights reserved.
 
 =head1 LICENSE
 
