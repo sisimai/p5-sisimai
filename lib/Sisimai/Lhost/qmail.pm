bc63d46fc88acffa0c5854485136d99cd565c4e1
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-08 07:54:57 +0900

    #672 Refer constants defined in Sisimai::Eb

--- lib/Sisimai/Lhost/qmail.pm
+++ lib/Sisimai/Lhost/qmail.pm
@@ -33,6 +33,7 @@ sub inquire {
     }
     return undef if $proceedsto == 0;
 
+    require Sisimai::Eb;
     require Sisimai::SMTP::Command;
     state $indicators = __PACKAGE__->INDICATORS;
     state $boundaries = [
@@ -77,11 +78,11 @@ sub inquire {
         # qmail-remote.c:231|  if (smtpcode() != 250) quit("ZConnected to "," but my name was rejected");
         "EHLO" => [" but my name was rejected."],
         # qmail-remote.c:238|  if (code >= 500) quit("DConnected to "," but sender was rejected");
-        # reason = rejected
+        # reason = Rejected
         "MAIL" => [" but sender was rejected."],
         # qmail-remote.c:249|  out("h"); outhost(); out(" does not like recipient.\n");
         # qmail-remote.c:253|  out("s"); outhost(); out(" does not like recipient.\n");
-        # reason = userunknown
+        # reason = UserUnknown
         "RCPT" => [" does not like recipient."],
         # qmail-remote.c:265|  if (code >= 500) quit("D"," failed on DATA command");
         # qmail-remote.c:266|  if (code >= 400) quit("Z"," failed on DATA command");
@@ -92,8 +93,8 @@ sub inquire {
 
     state $messagesof = {
         # notqmail 1.08 returns the following error message when the destination MX is NullMX
-        "notaccept"   => ["Sorry, I couldn't find a mail exchanger or IP address"],
-        "userunknown" => ["no mailbox here by that name"],
+        $Sisimai::Eb::Re00MX => ["Sorry, I couldn't find a mail exchanger or IP address"],
+        $Sisimai::Eb::ReUSER => ["no mailbox here by that name"],
     };
 
     my $dscontents = [__PACKAGE__->DELIVERYSTATUS]; my $v = undef;
@@ -162,7 +163,7 @@ sub inquire {
         # Detect the reason of bounce
         if( $e->{"command"} eq "HELO" || $e->{"command"} eq "EHLO" ) {
             # HELO | Connected to 192.0.2.135 but my name was rejected.
-            $e->{"reason"} = "blocked";
+            $e->{"reason"} = $Sisimai::Eb::ReBLOC;
 
         } else {
             # The error message includes any of patterns defined in the variable avobe
