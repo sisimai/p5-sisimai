841ef57043acb810d41ee8eb968b63f65927c677
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-08 04:27:19 +0900

    #672 Refer Sisimai::Eb::Re___1

--- lib/Sisimai/Reason/OnHold.pm
+++ lib/Sisimai/Reason/OnHold.pm
@@ -2,8 +2,9 @@ package Sisimai::Reason::OnHold;
 use v5.26;
 use strict;
 use warnings;
+use Sisimai::Eb;
 
-sub text  { 'onhold' }
+sub text  { $Sisimai::Eb::Re___1 }
 sub description { 'Sisimai could not decided the reason due to there is no (or less) detailed information for judging the reason' }
 sub match {
     # Try to match that the given text and regular expressions
@@ -17,15 +18,15 @@ sub match {
 sub true  {
     # On hold, Could not decide the bounce reason...
     # @param    [Sisimai::Fact] argvs   Object to be detected the reason
-    # @return   [Integer]               1: Status code is "onhold"
-    #                                   0: is not "onhold"
+    # @return   [Integer]               1: Status code is "OnHold"
+    #                                   0: is not "OnHold"
     # @since v4.1.28
     # @see http://www.ietf.org/rfc/rfc2822.txt
     my $class = shift;
     my $argvs = shift // return 0; return 0 unless $argvs->{'deliverystatus'};
 
-    return 1 if $argvs->{'reason'} eq 'onhold';
-    return 1 if (Sisimai::SMTP::Status->name($argvs->{'deliverystatus'}) || '') eq 'onhold';
+    return 1 if $argvs->{'reason'} eq $Sisimai::Eb::Re___1;
+    return 1 if (Sisimai::SMTP::Status->name($argvs->{'deliverystatus'}) || '') eq $Sisimai::Eb::Re___1;
     return 0
 }
 
@@ -36,7 +37,7 @@ __END__
 
 =head1 NAME
 
-Sisimai::Reason::OnHold - Bounce reason is C<onhold> or not.
+Sisimai::Reason::OnHold - Bounce reason is C<OnHold> or not.
 
 =head1 SYNOPSIS
 
@@ -45,17 +46,17 @@ Sisimai::Reason::OnHold - Bounce reason is C<onhold> or not.
 
 =head1 DESCRIPTION
 
-C<Sisimai::Reason::OnHold> checks the bounce reason is C<onhold> or not. This class is called only
-C<Sisimai::Reason> class. Sisimai will set C<onhold> to the reason of email bounce if there is no
+C<Sisimai::Reason::OnHold> checks the bounce reason is C<OnHold> or not. This class is called only
+C<Sisimai::Reason> class. Sisimai will set C<OnHold> to the reason of email bounce if there is no
 (or less) detailed information about email bounce for judging the reason.
 
 =head1 CLASS METHODS
 
 =head2 C<B<text()>>
 
-C<text()> method returns the fixed string C<onhold>.
+C<text()> method returns the fixed string C<OnHold>.
 
-    print Sisimai::Reason::OnHold->text;  # onhold
+    print Sisimai::Reason::OnHold->text;  # OnHold
 
 =head2 C<B<match(I<string>)>>
 
@@ -65,7 +66,7 @@ C<match()> method returns C<1> if the argument matched with patterns defined in 
 
 =head2 C<B<true(I<Sisimai::Fact>)>>
 
-C<true()> method returns C<1> if the bounce reason is C<onhold>. The argument must be C<Sisimai::Fact>
+C<true()> method returns C<1> if the bounce reason is C<OnHold>. The argument must be C<Sisimai::Fact>
 object and this method is called only from C<Sisimai::Reason> class.
 
 =head1 AUTHOR
@@ -74,7 +75,7 @@ azumakuniyuki
 
 =head1 COPYRIGHT
 
-Copyright (C) 2014-2016,2018,2020,2021,2024,2025 azumakuniyuki, All rights reserved.
+Copyright (C) 2014-2016,2018,2020,2021,2024-2026 azumakuniyuki, All rights reserved.
 
 =head1 LICENSE
 
