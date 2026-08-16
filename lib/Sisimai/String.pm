65a4763a7e1bf3315cbd7b50d6a121c551718a87
azumakuniyuki <azumakuniyuki@gmail.com>
2026-08-16 17:04:21 +0900

    (v5) Fix ReDoS vulnerability in Sisimai::String->to_plain reported from @arpitjain099

--- lib/Sisimai/String.pm
+++ lib/Sisimai/String.pm
@@ -80,8 +80,10 @@ sub to_plain {
         # 2. Remove <style>...</style>
         # 3. <a href = 'http://...'>...</a> to " http://... "
         # 4. <a href = 'mailto:...'>...</a> to " Value <mailto:...> "
-        s|<head>.+</head>||gsim,
-        s|<style.+?>.+</style>||gsim,
+        my $cv =  lc $plain;
+        $plain =~ s|<head>.+</head>||gsim      if index($cv, '</head>')  > -1;
+        $plain =~ s|<style.+?>.+</style>||gsim if index($cv, '</style>') > -1;
+
         s|<a\s+href\s*=\s*['"](https?://.+?)['"].*?>(.*?)</a>| [$2]($1) |gsim,
         s|<a\s+href\s*=\s*["']mailto:([^\s]+?)["']>(.*?)</a>| [$2](mailto:$1) |gsim,
         s/<[^<@>]+?>\s*/ /g,    # Delete HTML tags except <neko@example.jp>
