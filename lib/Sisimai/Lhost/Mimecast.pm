e393195abdbef55f40ba27e105401ed647290af7
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-23 06:52:17 +0900

    #691 Returns undef if the return value of Sisimai::RFC5322->part() is undef

--- lib/Sisimai/Lhost/Mimecast.pm
+++ lib/Sisimai/Lhost/Mimecast.pm
@@ -28,10 +28,10 @@ sub inquire {
     state $boundaries = ['Content-Type: :message/rfc822']; # No such line in lhost-mimecast-*
     state $startingof = {'message' => ['-- ']};
 
+    my $emailparts = Sisimai::RFC5322->part($mbody, $boundaries); return undef unless $emailparts;
     my $fieldtable = Sisimai::RFC1894->FIELDTABLE;
-    my $permessage = {};    # (Hash) Store values of each Per-Message field
     my $dscontents = [__PACKAGE__->DELIVERYSTATUS]; my $v = undef;
-    my $emailparts = Sisimai::RFC5322->part($mbody, $boundaries);
+    my $permessage = {};    # (Hash) Store values of each Per-Message field
     my $readcursor = 0;     # (Integer) Points the current cursor position
     my $recipients = 0;     # (Integer) The number of 'Final-Recipient' header
 
