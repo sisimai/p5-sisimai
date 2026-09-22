c9081e07cf3cea4415f1888eafeaf7486faa835f
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-23 07:09:29 +0900

    #691 Returns undef if the return value of Sisimai::RFC5322->part() is undef

--- lib/Sisimai/Lhost/X6.pm
+++ lib/Sisimai/Lhost/X6.pm
@@ -23,8 +23,8 @@ sub inquire {
     state $boundaries = ['The attachment contains the original mail headers'];
     state $startingof = {'message' => ['We had trouble delivering your message. Full details follow:']};
 
+    my $emailparts = Sisimai::RFC5322->part($mbody, $boundaries); return undef unless $emailparts;
     my $dscontents = [__PACKAGE__->DELIVERYSTATUS]; my $v = undef;
-    my $emailparts = Sisimai::RFC5322->part($mbody, $boundaries);
     my $readcursor = 0;     # (Integer) Points the current cursor position
     my $recipients = 0;     # (Integer) The number of 'Final-Recipient' header
 
