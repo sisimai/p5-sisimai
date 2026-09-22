cce726961ae90ea19ab8443d96c0e210070d566a
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-23 07:08:09 +0900

    #691 Returns undef if the return value of Sisimai::RFC5322->part() is undef

--- lib/Sisimai/Lhost/X2.pm
+++ lib/Sisimai/Lhost/X2.pm
@@ -30,8 +30,8 @@ sub inquire {
         ]
     };
 
+    my $emailparts = Sisimai::RFC5322->part($mbody, $boundaries); return undef unless $emailparts;
     my $dscontents = [__PACKAGE__->DELIVERYSTATUS]; my $v = undef;
-    my $emailparts = Sisimai::RFC5322->part($mbody, $boundaries);
     my $readcursor = 0;     # (Integer) Points the current cursor position
     my $recipients = 0;     # (Integer) The number of 'Final-Recipient' header
 
